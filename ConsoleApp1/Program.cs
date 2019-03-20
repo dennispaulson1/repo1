b77e295d6621badff871811829285bd7f6a009dc
Dénes Pálfi <dennis.paulson.1@gmail.com>
2019-03-20 22:21:56 +0100

    Módosítás próba

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //modosit
+            Console.WriteLine("Hello mivan?");
+            Console.ReadLine();
         }
     }
 }
