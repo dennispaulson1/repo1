333e0be2da9467d31545a6589b2092b3c766b233
Dénes Pálfi <dennis.paulson.1@gmail.com>
2019-03-20 23:08:46 +0100

    #2 recept program

---
+++ ConsoleApp_receptek/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_receptek
+{
+    /// <summary>
+    /// Pörkölt recept hozzávalóit kiírjuk
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Marhahús");
+            Console.WriteLine("Vöröshagyma");
+            Console.WriteLine("Olaj");
+            Console.WriteLine("Pirospaprika");
+
+            Console.ReadLine();
+        }
+    }
+}
